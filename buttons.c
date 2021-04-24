ac5f50d81267c989e27af28c326e2a2178c6764c
yasirfaizahmed <yasirfaizahmed.n@gmail.com>
2021-04-24 16:19:05 +0530

    PORTB PIN4 is not working

--- buttons.c
+++ buttons.c
@@ -3,19 +3,19 @@
 //#include "inc/GPIO_DRIVER0.h"
 
 #define Display_CDD     DDB5
-#define Button_Sensor   DDB4
-#define Heater          DDB3
-#define Temp_Sensor     DDB2
+#define Button_Sensor   DDB3	//something is wrong with my PORTB PIN4, so had to take PIN3
+#define Heater          DDB2
+#define Temp_Sensor     DDB1
 
 
 int main(void)
 {
     DDRB |= _BV(Display_CDD); // as input
     DDRB &= ~(_BV(Button_Sensor)); // Configuring PB4 as Input
-	PORTB &= ~(_BV(Button_Sensor));	// no internal pull-up
+	//PORTB &= ~(_BV(Button_Sensor));	// no internal pull-up
 	DDRB &= ~(_BV(Heater));	// as input
-	PORTB &= ~(_BV(Heater));	//no internal pull-up
-    MCUCR |= (_BV(PUD));	//tri-state
+	//PORTB &= ~(_BV(Heater));	//no internal pull-up
+    //MCUCR |= (_BV(PUD));	//tri-state
     while (1)
     {
         if ( ( ((PINB&(_BV(Button_Sensor)))>>Button_Sensor) == 1 ) && ( ((PINB&(_BV(Heater)))>>Heater) == 1 ) ){
